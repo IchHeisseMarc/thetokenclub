fe78769f876b6ffb79597e702a47c2785ce043fb
IchHeisseMarc <40389754+IchHeisseMarc@users.noreply.github.com>
2021-03-28 10:52:54 +0200

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,12 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-  // Magnific popup calls
+  $('[data-countdown]').each(function() {
+    var $this = $(this), finalDate = $(this).data('countdown');
+    $this.countdown(finalDate, function(event) {
+      $this.html(event.strftime('%D days %H:%M:%S'));
+    });
+  });
   
 
 })(jQuery); // End of use strict
